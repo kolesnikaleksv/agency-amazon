0da8d666fadfdee6f55ee37e94335c69dbba6aff
kolesnikaleksv <kolesnikaleksv@gmail.com>
2024-01-26 08:24:31 +0200

    Add closeButton component, change navigationMenu

---
+++ src/components/CloseButton/CloseButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './close-button.scss';
+interface OnClose {
+  onClose: () => {}
+}
+const CloseButton: React.FC<OnClose> = ({onClose}) => {
+  return (
+    <div className="close-button" onClick={() => onClose()}>
+        <div className="close-button__circle">
+          <span className="close-button__circle close-button__circle--line-1"></span>
+          <span className="close-button__circle close-button__circle--line-2"></span>
+        </div>
+    </div>
+  )
+}
+
+export default CloseButton;
--- src/components/NavigationMenu/NavigationMenu.tsx
+++ src/components/NavigationMenu/NavigationMenu.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Accounts from '../Accounts/Accounts';
+import CampaignPage from '../CampaignPage/CampaignPage';
+import Profiles from '../Profiles/profiles';
+import Campaigns from '../Campaigns/Campaigns';
 
 const NavigationMenu = () => {
   const location = useLocation();
@@ -23,6 +26,9 @@ const NavigationMenu = () => {
       >
       <Routes location={displayLocation}>
         <Route path="/" element={<Accounts/>} />
+        <Route path='campaigns/:id' element={<CampaignPage />} />
+        <Route path='profiles' element={<Profiles />} />
+        <Route path='campaigns' element={<Campaigns/>} />
       </Routes>
     </div>
   )
